2fae0c2b943cf440aa9e50986a5e09c8a476a51c
jgaxmey <jiagame1@126.com>
2021-08-21 23:35:23 +0800

    子弹

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,10 +9,13 @@ class AlienInvasion:
         '''初始化游戏并创建游戏资源'''
         pygame.init()
         self.settings = Settings()
-        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
-        self.settings.screen_width = self.screen.get_rect().width
-        self.settings.screen_height = self.screen.get_rect().height
-        pygame.display.set_caption("Alien Invasion")
+        # self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        # self.settings.screen_width = self.screen.get_rect().width
+        # self.settings.screen_height = self.screen.get_rect().height
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width,self.settings.screen_height)
+        )
+        pygame.display.set_caption("消灭外星人")
         self.ship = Ship(self)
         self.bullets = pygame.sprite.Group()
 
@@ -21,7 +24,7 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self.bullets.update()
+            self._update_bullets()
             self._update_screen()
             self.screen.fill(self.settings.bg_color)
             self.ship.blitme()
@@ -54,8 +57,17 @@ class AlienInvasion:
             self.ship.moving_left = False    
     
     def _fire_bullet(self):
-        new_bullet = Bullet(self)
-        self.bullets.add(new_bullet)
+        if len(self.bullets) < self.settings.bullet_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+    def _update_bullets(self):
+        self.bullets.update()
+        #删除消失的子弹
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom <=0:
+                self.bullets.remove(bullet)
+
 
     def _update_screen(self):
         self.screen.fill(self.settings.bg_color)
